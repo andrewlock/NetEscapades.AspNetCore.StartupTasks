e1514e244f4fb08f50211e1ca90a75131b6f41db
Andrew Lock <andrewlock.net@gmail.com>
2018-11-03 19:22:49 +0000

    Add sample

---
+++ samples/UsageSample/MigrationStartupTask.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NetEscapades.AspNetCore.StartupTasks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UsageSample.Data;
+
+namespace UsageSample
+{
+    public class MigrationStartupTask : IStartupTask
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public MigrationStartupTask(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                await dbContext.Database.MigrateAsync();
+            }
+        }
+    }
+}
---
+++ samples/UsageSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace UsageSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunWithTasksAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
