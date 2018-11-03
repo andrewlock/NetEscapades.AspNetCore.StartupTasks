11ddb38a7e3e1d5b30733b2e509ae777072d6648
Andrew Lock <andrewlock.net@gmail.com>
2018-11-03 18:53:15 +0000

    Add library code

---
+++ src/NetEscapades.AspNetCore.StartupTasks/IStartupTask.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetEscapades.AspNetCore.StartupTasks
+{
+    /// <summary>
+    /// A task to execute before starting the application
+    /// </summary>
+    public interface IStartupTask
+    {
+        /// <summary>
+        /// Execute the startup task, before the WebHost is run
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token for cancelling the task</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task ExecuteAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/NetEscapades.AspNetCore.StartupTasks/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NetEscapades.AspNetCore.StartupTasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions for registering <see cref="IStartupTask"/>s with an <see cref="IServiceCollection"/>
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add an <see cref="IStartupTask"/> registration for the given type.
+        /// </summary>
+        /// <typeparam name="TStartupTask">An <see cref="IStartupTask"/> to register.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to register with.</param>
+        /// <returns>The original <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddStartupTask<TStartupTask>(this IServiceCollection services)
+            where TStartupTask : class, IStartupTask
+            => services.AddTransient<IStartupTask, TStartupTask>();
+    }
+}
